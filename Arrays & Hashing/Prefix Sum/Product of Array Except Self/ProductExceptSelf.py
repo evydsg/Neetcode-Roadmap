bbd04cf861b8fc2277cc82580c3498af41163f84
Evelise Karen Guenda <evelisekguenda@gmail.com>
2025-02-11 06:42:57 -0800

    ProductExceptSelf

---
+++ Arrays & Hashing/Prefix Sum/Product of Array Except Self/ProductExceptSelf.py
@@ -0,0 +1,16 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        prefix = 1
+
+        #Prefix Product
+        for index, num in enumerate(nums):
+            prefix *= num
+            nums[index] = prefix
+        
+        postfix = 1
+        for index in range(len(nums)-1, -1, -1):
+            postfix *= num if index < len(nums)-1 else 1
+            nums[index] *= postfix
+        
+        
+        
