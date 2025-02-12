e98f83897343b61a932ea8692ad531b30c10cfd1
Evelise Karen Guenda <evelisekguenda@gmail.com>
2025-02-12 06:37:52 -0800

    ProductExceptSelf

--- Arrays & Hashing/Prefix Sum/Product of Array Except Self/ProductExceptSelf.py
+++ Arrays & Hashing/Prefix Sum/Product of Array Except Self/ProductExceptSelf.py
@@ -1,16 +1,34 @@
+from typing import List
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         prefix = 1
+        prefix_array = []
+        postfix_array = []
+        output_array = [1] * len(nums)  # Fix incorrect initialization
 
-        #Prefix Product
-        for index, num in enumerate(nums):
+        # Prefix Product Calculation
+        for num in nums:
+            prefix_array.append(prefix)
             prefix *= num
-            nums[index] = prefix
-        
+
+        # Postfix Product Calculation
         postfix = 1
-        for index in range(len(nums)-1, -1, -1):
-            postfix *= nums[index] if index < len(nums)-1 else 1
-            nums[index] *= postfix
+        for index in range(len(nums) - 1, -1, -1):
+            postfix_array.append(postfix)
+            postfix *= nums[index]
+
+        # Reverse postfix_array to align with prefix_array
+        postfix_array.reverse()
+
+        # Compute Final Output Array
+        for index in range(len(nums)):
+            output_array[index] = prefix_array[index] * postfix_array[index]
+
+        return output_array
+
+
+
         
         
         
---
+++ Arrays & Hashing/Prefix Sum/Product of Array Except Self/ProductExceptSelfII.py
@@ -0,0 +1,14 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        result = [1] * len(nums)
+        prefix = postfix = 1
+
+        for index in range(len(nums)):
+            result[index] = prefix
+            prefix *= nums[index]
+        
+        for index in range(len(nums)-1, -1, -1):
+            result[index] *= postfix
+            postfix *= nums[index]
+        
+        return result
