fbca96c8aa0c8f214664064e30daea9dd1899676
Evelise Karen Guenda <evelisekguenda@gmail.com>
2025-02-25 06:04:59 -0800

    TopKFrequentII

---
+++ Arrays & Hashing/Practice/Top K Frequenet Elements/topKFrequentII.py
@@ -0,0 +1,19 @@
+ class Solution:
+    def topKFrequent(self, nums: List[int]) -> List[int]:
+        count = {}
+        frequency =[[] for index in range(len(nums) + 1)]
+
+        for number in nums:
+            count[number] += 1 + count.get(number, 0)
+
+        for number, freq in count.items():
+            frequency[freq].append(number)
+        
+        result = []
+
+        for index in range(len(nums)-1, 0, -1):
+            for number in frequency[index]:
+                result.append(number)
+
+                if len(result) == k:
+                    return result
